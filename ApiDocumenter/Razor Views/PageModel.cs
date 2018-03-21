8a4148783cde52e7d727c0e284e5ba2ed7439a50
lbrooks3479 <lbrooks3479@gmail.com>
2018-03-20 20:20:29 -0400

    adding razor view

---
+++ ApiDocumenter/Razor Views/PageModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using ApiDocumentor.Models;
+using System.Linq;
+
+namespace ApiDocumentor.PageModel
+{
+    public class PageModel
+    {
+        public string ApiId { get; set; }
+        public List<ApiControllerInformation> ApiInfo { get; set; }
+        public ApiControllerInformation CurrentApi { get; set; }
+        public MethodInformation CurrentMethod { get; set; }
+
+        public PageModel(List<ApiControllerInformation> apiInformation, string apiId, string controllerName = "", string methodName = "", string attributeId = "") 
+        {
+            ApiId = apiId;
+            ApiInfo = apiInformation;
+            CurrentMethod = new MethodInformation();
+
+            if (!String.IsNullOrEmpty(controllerName))
+            {
+                CurrentApi = ApiInfo.Where(x => x.ControllerName == controllerName).FirstOrDefault();
+
+                if (!String.IsNullOrEmpty(methodName))
+                {
+                    CurrentMethod = CurrentApi.Methods.Where(x => x.Name == methodName && x.Attributes.Contains(attributeId)).FirstOrDefault();
+                }
+                else
+                {
+                    CurrentMethod = new MethodInformation { Name = "METHOD NOT FOUND" };
+                }
+            }
+            else
+            {
+                CurrentApi = new ApiControllerInformation();
+            }
+        }
+    }
+}
